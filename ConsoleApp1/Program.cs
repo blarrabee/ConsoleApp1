1536e00501062db14a4adceb81c6881d7455d451
[Blaise Larrabee] <blaiselarrabee@gmail.com>
2020-12-01 20:16:53 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("my name is Blaise");
+
+            System.Int32 anyNumber = 10;
+            System.String simpleString = "simple string";
+            //Console.WriteLine(anyNumber.GetTypeCode);
+
+            //anyNumber = 4;
+
+            Console.WriteLine(anyNumber);
+            Thread.Sleep(1000);
+            Console.WriteLine("done sleeping");
+            Console.WriteLine(simpleString);
+
+        }
+    }
+}
